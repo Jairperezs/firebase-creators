0851b01de55e7cd43464059b359fb39bb3575243
Jair Pérez <jair.perez@outlook.com>
2017-12-04 16:02:13 -0500

    Add firebase

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,120 @@
 import React, { Component } from 'react'
+import * as firebase from 'firebase'
 
 class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            message: null,
+            verify: null
+        }
+    }
+
+    componentWillMount() {
+        firebase.auth().onAuthStateChanged((user) => {
+            if(user) {
+                this.setState({verify: 'Estas logeado'})
+            } else {
+                this.setState({verify: 'No estas logeado'})
+            }
+        })
+    }
+    SignUp(e) {
+        e.preventDefault()
+        firebase.auth().createUserWithEmailAndPassword(this.emailSignUp.value, this.passwordSignUp.value)
+            .then(({uid}) => {
+                localStorage.setItem("uid", uid)
+                return this.setState({message: 'Te has registrado correctamente'})
+            })
+            .catch(error => {
+                // Hanlde Errors here.
+                let errorCode = error.code
+                let errorMessage = error.message
+                console.log(errorCode)
+                return this.setState({message: errorMessage})
+            })
+    }
+
+    SignIn(e) {
+        e.preventDefault()
+        firebase.auth().signInWithEmailAndPassword(this.email.value, this.password.value)
+            .then(({uid, displayName}) => {
+                localStorage.setItem('uid', uid)
+                return this.setState({message: `Has iniciado sesion correctamente, ${displayName ? displayName : 'por favor edita tu nombre'}`})
+            })
+            .catch(error => {
+                let errorCode = error.code
+                let errorMessage = error.message
+                console.log(errorCode)
+                this.setState({message: errorMessage})
+            })
+    }
+    logout(e) {
+        e.preventDefault()
+        firebase.auth().signOut()
+            .then(()=> {
+                localStorage.removeItem('uid')
+                return this.setState({message: 'Has cerrado sesion correctamente'})
+            })
+            .catch(error => console.log(error.message))
+    }
+    updateName (e) {
+        e.preventDefault()
+        firebase.database().ref('users/' + localStorage.uid).set({
+            displayName: this.displayName.value
+        })
+    }
     render() {
         return (
-            <h1> Mi app</h1>
+            <div className="Container">
+                <div className="signup-content">
+                    <h1>Registro</h1>
+                    <form onSubmit={this.SignUp.bind(this)}>
+                        <div className="signup-item">
+                            <input type="email" placeholder="Correo electronico" ref={emailRef => this.emailSignUp = emailRef} />
+                        </div>
+                        <div className="signup-item">
+                            <input type="password" placeholder="Contraseña" ref={passwordRef => this.passwordSignUp = passwordRef} />
+                        </div>
+                        <div className="signup-button">
+                            <button>Registrar</button>
+                        </div>
+                    </form>
+                </div>
+                <div className="signin-content">  
+                    <h1>Iniciar sesion</h1>
+                    <form onSubmit={this.SignIn.bind(this)}>
+                        <div className="signin-item">
+                            <input type="email" placeholder="Correo electronico" ref={emailRef => this.email = emailRef} />
+                        </div>
+                        <div className="signin-item">
+                            <input type="password" placeholder="Contraseña" ref={passwordRef => this.password = passwordRef} />
+                        </div>
+                        <div className="signin-item">
+                            <button>Inisiar sesion</button>
+                        </div>
+                    </form>
+                </div>
+                {localStorage.uid ?
+                    (
+                        <div className="update-content">
+                            <h1>Editar nombre</h1>
+                            <p>* Tienes que estar logeado</p>
+                            <form onSubmit={this.updateName.bind(this)}>
+                                <div className="update-item">
+                                    <input type="text" placeholder="Nombre completo" ref={displayName => this.displayName = displayName}/>
+                                </div>
+                                <div className="update-button">
+                                    <button>Actualizar nombre</button>
+                                </div>
+                            </form>
+                        </div>
+                    ) : null}
+                <div className="message-content">
+                    <p>{this.state.message}</p>
+                </div>
+                {localStorage.uid ? <button onClick={this.logout.bind(this)}>Cerrar sesion</button>: null}
+            </div>
         )
     }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,19 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import * as firebase  from 'firebase'
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyBRu9imf5_2f3zFaaThJhOAk9e8D3_o9a4",
+  authDomain: "test-crud-ab6ab.firebaseapp.com",
+  databaseURL: "https://test-crud-ab6ab.firebaseio.com",
+  projectId: "test-crud-ab6ab",
+  storageBucket: "test-crud-ab6ab.appspot.com",
+  messagingSenderId: "858148934912"
+};
+
+firebase.initializeApp(config);
+
 
 import App from './components/App.jsx'
 
